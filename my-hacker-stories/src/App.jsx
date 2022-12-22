2fa79370fe2af62c3d1837b7e16f07aeb615106e
marcelrm11 <marcelrm11@gmail.com>
2022-12-22 14:05:52 +0100

    end section 5, jsx

--- my-hacker-stories/src/App.jsx
+++ my-hacker-stories/src/App.jsx
@@ -1,11 +1,37 @@
 import * as React from "react";
 
-const title = "React";
+const welcome = {
+  title: "React",
+  greeting: "Hey",
+};
+const football = {
+  players: [
+    "Messi",
+    "Cruyff",
+    "Maradona",
+    "Pelé",
+    "Ronaldinho",
+    "Rivaldo",
+    "Ronaldo",
+    "Xavi",
+    "Iniesta",
+  ],
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <h2>Football Players</h2>
+      <ul>
+        {football.players.map((player) => (
+          <li>{player}</li>
+        ))}
+      </ul>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
     </div>
   );
 }
