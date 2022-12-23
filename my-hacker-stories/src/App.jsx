5287b745e38da6059c834bcef88be843bdf9c272
marcelrm11 <marcelrm11@gmail.com>
2022-12-23 08:24:13 +0100

    end section 6 lists

--- my-hacker-stories/src/App.jsx
+++ my-hacker-stories/src/App.jsx
@@ -18,20 +18,54 @@ const football = {
   ],
 };
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <h1>
         {welcome.greeting} {welcome.title}
       </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
+      <hr />
       <h2>Football Players</h2>
       <ul>
         {football.players.map((player) => (
-          <li>{player}</li>
+          <li key={player}>{player}</li>
         ))}
       </ul>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
+      <hr />
+      <h2>My Hacker Stories</h2>
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
