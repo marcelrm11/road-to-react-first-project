d9000f2547417ea0fc9315fffba4a2a65747c799
marcelrm11 <marcelrm11@gmail.com>
2022-12-30 12:43:33 +0100

    end section normal css, before exercises

--- my-hacker-stories/src/App.jsx
+++ my-hacker-stories/src/App.jsx
@@ -102,14 +102,13 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
-      <hr />
       {stories.isError && <p>Something went wrong...</p>}
       {stories.isLoading ? (
         <p>loading stories...</p>
@@ -122,7 +121,7 @@ const App = () => {
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
   return (
-    <form onSubmit={onSearchSubmit}>
+    <form onSubmit={onSearchSubmit} className="search-form">
       <InputWithLabel
         id="search"
         value={searchTerm}
@@ -131,7 +130,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => {
       >
         <strong>Search: </strong>
       </InputWithLabel>
-      <button type="submit" disabled={!searchTerm}>
+      <button
+        type="submit"
+        disabled={!searchTerm}
+        className="button button_large"
+      >
         Submit
       </button>
     </form>
@@ -161,14 +164,22 @@ const Item = ({
   onRemoveItem,
 }) => {
   return (
-    <li>
-      <span>
+    <li className="item">
+      <span style={{ width: "40%" }}>
         <a href={url}>{title}</a>
       </span>
-      <span>{author}</span>
-      <span>{num_comments}</span>
-      <span>{points}</span>
-      <button onClick={() => onRemoveItem(objectID)}>Remove</button>
+      <span style={{ width: "30%" }}>{author}</span>
+      <span style={{ width: "10%" }}>{num_comments}</span>
+      <span style={{ width: "10%" }}>{points}</span>
+      <span style={{ width: "10%" }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(objectID)}
+          className="button button_small"
+        >
+          Dismiss
+        </button>
+      </span>
     </li>
   );
 };
@@ -186,7 +197,10 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
+      &nbsp;
       <input
         id={id}
         type={type}
@@ -194,6 +208,7 @@ const InputWithLabel = ({
         onChange={onInputChange}
         autoComplete={autoComplete}
         autoFocus={isFocused}
+        className="input"
       ></input>
     </>
   );
